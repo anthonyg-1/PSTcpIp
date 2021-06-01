64a14df1622c42cc5cfda03f2acf1b7030297918
Tony Guimelli <tony.guimelli@gmail.com>
2021-06-01 08:39:52 -0400

    Update PSTcpIp.psm1

--- PSTcpIp/PSTcpIp.psm1
+++ PSTcpIp/PSTcpIp.psm1
@@ -517,19 +517,22 @@ function Get-TlsStatus {
             $handshakeSucceeded = $true
 
             # Get HTTP Strict Transport Security values:
-            [string]$strictTransportSecurityValue = ""
+            [string]$strictTransportSecurityValue = "No value specified for strict transport security (HSTS)"
             try {
                 $webRequestResponse = Invoke-WebRequest -Uri $targetUri -MaximumRedirection 0 -ErrorAction Stop
 
                 [HashTable]$responseHeaders = $webRequestResponse.Headers
 
                 $strictTransportSecurityValue = $responseHeaders['Strict-Transport-Security']
+
+                if ($strictTransportSecurityValue.Length -lt 1) {
+                    $strictTransportSecurityValue = "Strict-Transport-Security not found in header"
+                }
             }
             catch {
                 $strictTransportSecurityValue = "Unable to acquire HSTS value"
             }
             $tlsStatus.StrictTransportSecurity = $strictTransportSecurityValue
-
         }
         catch {
             $cryptographicExceptionMessage = "Unable to establish SSL handshake with the following host: {0}" -f $targetHost
