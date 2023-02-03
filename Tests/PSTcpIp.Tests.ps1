7b009da425365bc16c28cf3e1fa5b73d11fbc151
Tony Guimelli <tony.guimelli@gmail.com>
2023-02-03 16:58:34 -0500

    Update PSTcpIp.Tests.ps1

--- Tests/PSTcpIp.Tests.ps1
+++ Tests/PSTcpIp.Tests.ps1
@@ -56,7 +56,7 @@ Describe "Testing module and cmdlets against PSSA rules" -Tag Unit -WarningActio
     Context "$module test against PSSA rules" {
         $modulePath = "$moduleDirectory\$module.psm1"
 
-        $analysis = Invoke-ScriptAnalyzer -Path $modulePath
+        $analysis = Invoke-ScriptAnalyzer -Path $modulePath -ExcludeRule PSUseBOMForUnicodeEncodedFile
 
         foreach ($rule in $scriptAnalyzerRules) {
             It "should pass $rule" {
