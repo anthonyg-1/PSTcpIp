53a69e1c6c2fc7f15a9d0e8c311100782a9d6bf4
Tony Guimelli <tony.guimelli@gmail.com>
2024-05-02 18:38:55 -0400

    Add files via upload

--- PSTcpIp/PSTcpIp.psm1
+++ PSTcpIp/PSTcpIp.psm1
@@ -1,4 +1,4 @@
-using namespace System
+﻿using namespace System
 using namespace System.Collections
 using namespace System.Net
 using namespace System.Net.Sockets
@@ -214,27 +214,27 @@ function Get-WebServerCertificate([string]$TargetHost, [int]$Port = 443, [int]$T
             # Build target host and part for connect argument for openssl:
             $targetHostAndPort = "{0}:{1}" -f $TargetHost, $Port
 
-            #try {
-            # Get the cert:
-            $openSslResult = "Q" | openssl s_client -connect $targetHostAndPort 2>$null
+            try {
+                # Get the cert:
+                $openSslResult = "Q" | openssl s_client -connect $targetHostAndPort 2>$null
 
-            # Parse the relevant base64 cert resulting from openssl:
-            $beginString = "BEGIN CERTIFICATE"
-            $endString = "END CERTIFICATE"
-            $base64CertString = (($openSslResult -join "").Split($beginString)[1].Split($endString)[0]).Replace("-", "")
+                # Parse the relevant base64 cert resulting from openssl:
+                $beginString = "BEGIN CERTIFICATE"
+                $endString = "END CERTIFICATE"
+                $base64CertString = (($openSslResult -join "").Split($beginString)[1].Split($endString)[0]).Replace("-", "")
 
-            # Convert the base64 string to a byte array to be fed to the X509Certificate2 constructor:
-            [byte[]]$certBytes = [System.Convert]::FromBase64String($base64CertString)
+                # Convert the base64 string to a byte array to be fed to the X509Certificate2 constructor:
+                [byte[]]$certBytes = [System.Convert]::FromBase64String($base64CertString)
 
-            # Instantiate the certificate from the deserialized byte array:
-            $tlsCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($certBytes)
+                # Instantiate the certificate from the deserialized byte array:
+                $tlsCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($certBytes)
 
-            # return the TLS cert:
-            return $tlsCert
-            #}
-            #catch {
-            #   throw $CryptographicException
-            #}
+                # return the TLS cert:
+                return $tlsCert
+            }
+            catch {
+                throw $CryptographicException
+            }
         }
         else {
             throw $CryptographicException
