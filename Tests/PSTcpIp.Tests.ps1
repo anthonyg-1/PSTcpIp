3aaf1f9011309e4d77c14bcbc8fbf75f80bf9a62
Tony Guimelli <tony.guimelli@gmail.com>
2024-10-01 12:50:37 -0400

    Update PSTcpIp.Tests.ps1

--- Tests/PSTcpIp.Tests.ps1
+++ Tests/PSTcpIp.Tests.ps1
@@ -63,7 +63,7 @@ Describe "Testing module and cmdlets" -Tag Unit -WarningAction SilentlyContinue 
     #>
 
     Context "$module test against PSSA rules" {
-        $analysis = Invoke-ScriptAnalyzer -Path $modulePath -ExcludeRule PSReviewUnusedParameter, PSAvoidUsingEmptyCatchBlock, PSAvoidUsingWriteHost
+        $analysis = Invoke-ScriptAnalyzer -Path $modulePath -ExcludeRule PSReviewUnusedParameter, PSAvoidUsingEmptyCatchBlock, PSAvoidUsingWriteHost, PSUseShouldProcessForStateChangingFunctions
 
         foreach ($rule in $scriptAnalyzerRules) {
             It "should pass $rule" {
